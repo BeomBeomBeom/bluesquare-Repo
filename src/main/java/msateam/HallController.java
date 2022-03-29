ae32664de33fe22c4797bc98cc304889f7b7687d
mshwang87 <mshwang87@naver.com>
2022-03-29 15:51:18 +0900

    mshwang87

--- src/main/java/msateam/HallController.java
+++ src/main/java/msateam/HallController.java
@@ -14,12 +14,30 @@ import java.util.List;
 
 
 
-@RequestMapping(value = "/chkAndReqReserve",
+@RequestMapping(value = "/checkReservation",
         method = RequestMethod.GET,
         produces = "application/json;charset=UTF-8")
 
 public void checkReservation(HttpServletRequest request, HttpServletResponse response)
         throws Exception {
         System.out.println("##### /hall/checkReservation  called #####");
-        }
+
+        // Parameter로 받은 SeatId 추출
+        long seatId = Long.valueOf(request.getParameter("seatId"));
+        System.out.println("######################## checkReservation seatId : " + seatId);
+
+        // SeatId 데이터 조회
+        Optional<Hall> res = seatRepository.findById(seatId);
+        Hall hall = res.get(); // 조회한 Hall seat 데이터
+        System.out.println("######################## checkReservation hall.getStatus() : " + hall.getStatus());
+
+        // room의 상태가 available이면 true
+        boolean result = false;
+        if(hall.getStatus().equals("available")) {
+                result = true;
+        } 
+
+        System.out.println("######################## checkReservation Return : " + result);
+        return result;
+
  }
